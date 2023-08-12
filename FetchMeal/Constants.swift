933d4be0b2d09e5cbf7a3063793f1256d3ccff27
Fernando Brito <fernando_britoa@hotmail.com>
2023-08-12 11:07:31 -0600

    Initial project setup

---
+++ FetchMeal/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  FetchMeal
+//
+//  Created by Fernando Brito on 12/08/23.
+//
+
+import Foundation
+
+struct K {
+    enum Colors {
+        static let backgroundBeige = "backgroundBeige"
+    }
+}
---
+++ FetchMeal/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FetchMeal
+//
+//  Created by Fernando Brito on 11/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(named: K.Colors.backgroundBeige)
+    }
+}
