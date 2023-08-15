ec8cc4db81a8bf958281489b104630f0eec326ec
Fernando Brito <fernando_britoa@hotmail.com>
2023-08-14 21:37:36 -0600

    Added UIViewController extension to show a toast, for error handling

--- FetchMeal/Resources/Extensions.swift
+++ FetchMeal/Resources/Extensions.swift
@@ -33,3 +33,49 @@ extension UIImageView {
         }
     }
 }
+
+enum ToastType {
+    case success
+    case error
+}
+
+extension UIViewController {
+    func showToast(message: String, type: ToastType) {
+        let padding = 5.0
+        let verticalOffset = 100.0
+        let horizontalOffset = 20.0
+        let bgColor = type == .success ? UIColor.black : UIColor.systemRed
+
+        let containerView = UIView(frame: CGRect(
+            x: horizontalOffset,
+            y: self.view.frame.size.height - verticalOffset,
+            width: self.view.frame.width - horizontalOffset * 2,
+            height: 40 + padding * 2))
+
+        let toastLabel = UILabel(frame: CGRect(
+            x: padding,
+            y: padding,
+            width: containerView.frame.width - padding * 2,
+            height: containerView.frame.height - padding * 2))
+
+        containerView.alpha = 1.0
+        containerView.clipsToBounds = true
+        containerView.layer.cornerRadius = 8
+        containerView.backgroundColor = bgColor.withAlphaComponent(0.9)
+
+        toastLabel.text = message
+        toastLabel.numberOfLines = 2
+        toastLabel.textAlignment = .center
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = .systemFont(ofSize: 12)
+
+        containerView.addSubview(toastLabel)
+        self.view.addSubview(containerView)
+
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            containerView.alpha = 0.0
+        }, completion: { _ in
+            containerView.removeFromSuperview()
+        })
+    }
+}
